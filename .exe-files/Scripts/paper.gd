09d065ec204c58bde05268bf64d24259aec502a2
Astralliz <138964750+Astralliz@users.noreply.github.com>
2025-11-18 19:44:17 +0800

    Added Stamp asset, changed world scene name to "work_area"

--- .exe-files/Scripts/paper.gd
+++ .exe-files/Scripts/paper.gd
@@ -32,7 +32,7 @@ func spawn_stamp() -> void:
 	if has_node("Stamp"):
 		return
 	
-	var stamp := preload("res://Scenes/stamp.tscn").instantiate()
+	var stamp := preload("res://Scenes/approve_stamp.tscn").instantiate()
 	stamp.name = "Stamp"
 	add_child(stamp)
 	stamp.global_position = get_global_mouse_position()
--- .exe-files/Scripts/work_area.gd
+++ .exe-files/Scripts/work_area.gd
@@ -4,10 +4,11 @@ var paper_stack: Array = []
 var paper_scene := preload("res://Scenes/paper.tscn")
 var dragged_paper: Node2D = null
 
+@onready var approve_btn: Button = $ApproveBtn
+@onready var decline_btn: Button = $DeclineBtn
+
 func _ready() -> void:
-	for i in range(2):
-		var paper = paper_scene.instantiate()
-		add_paper(paper)
+	var paper = paper_scene.instantiate()
 
 func add_paper(paper: Node2D) -> void:
 	add_child(paper)
