331e2869d584c2f8361a87775ad894c02cb06f35
Astralliz <138964750+Astralliz@users.noreply.github.com>
2025-11-18 13:25:43 +0800

    Project folder setup and add overlapping logic in paper.gd

--- .exe-files/Scripts/paper.gd
+++ .exe-files/Scripts/paper.gd
@@ -6,22 +6,26 @@ var mouseEntered = false
 func _ready() -> void:
 	$Area2D.mouse_entered.connect(onMouseEnter)
 	$Area2D.mouse_exited.connect(onMouseExit)
-	
+
 func onMouseEnter() -> void:
 	mouseEntered = true
-	
+
 func onMouseExit() -> void:
 	mouseEntered = false
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("rightClick") and mouseEntered:
-		spawn_stamp()
-	
+	# Drags document when clicked
 	if Input.is_action_just_pressed("click"):
 		offset = global_position - get_global_mouse_position()
 	
 	if Input.is_action_pressed("click") and mouseEntered:
 		global_position = get_global_mouse_position() + offset
+	if Input.is_action_just_pressed("click") and mouseEntered:
+		get_parent().bring_to_front(self)
+	
+	# Stamp logic
+	if Input.is_action_just_pressed("rightClick") and mouseEntered:
+		spawn_stamp()
 
 func spawn_stamp() -> void:
 	if has_node("Stamp"):
@@ -31,4 +35,3 @@ func spawn_stamp() -> void:
 	stamp.name = "Stamp"
 	add_child(stamp)
 	stamp.global_position = get_global_mouse_position()
-#kupal ka ba
---
+++ .exe-files/Scripts/world.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var paper_stack: Array = []
+var paper_scene := preload("res://Scenes/paper.tscn")
+
+func _ready() -> void:
+	for i in range(2):
+		var paper = paper_scene.instantiate()
+		add_paper(paper)
+
+func add_paper(paper: Node2D) -> void:
+	add_child(paper)
+	paper_stack.append(paper)
+	_update_z_indices()
+
+func bring_to_front(paper: Node2D) -> void:
+	if paper_stack.has(paper):
+		paper_stack.erase(paper)
+	
+	paper_stack.append(paper)
+	_update_z_indices()
+
+func _update_z_indices() -> void:
+	for i in range(paper_stack.size()):
+		paper_stack[i].z_index = i
