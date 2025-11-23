9a0d86d5479508f6ff6a9c35531c76406701ed49
admin <manueljosepera487@gmail.com>
2025-11-23 15:11:06 +0800

    latest update

--- .exe-files/Scripts/UI/story_controller.gd
+++ .exe-files/Scripts/UI/story_controller.gd
@@ -8,3 +8,7 @@ func _on_story_1_pressed() -> void:
 
 func _on_back_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu Scenes/story_menu.tscn")
+
+
+func _on_day_1_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu Scenes/Story Scene/day_1.tscn")
