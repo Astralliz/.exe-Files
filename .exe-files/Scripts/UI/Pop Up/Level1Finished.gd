7a21578ebb991e4b6782093f21337c28a5022f34
admin <manueljosepera487@gmail.com>
2025-12-03 22:00:34 +0800

    Added Mentor In other Dialogue

--- .exe-files/Scripts/UI/Pop Up/Level1Finished.gd
+++ .exe-files/Scripts/UI/Pop Up/Level1Finished.gd
@@ -16,16 +16,16 @@ func _ready() -> void:
 	Player_Data.unlock_achievement(achievement)
 	Player_Data.add_evaluates(added_eval)
 	Player_Data.add_questions(added_ques)
-	message.text = "Congrats🥳🥳!!! \n You Unlock an Achievement \n" +  achievement + "\nPlay More Day to Unlock more!"
+	message.text = "Congrats!\n You Unlock an\n Achievement \n'" +  achievement + "'\nPlay More Day\n to Unlock more!"
 
 
 func _on_achievement_pressed() -> void:
-	message.text = "CONGRATS🥳🥳!!! \n You get a " + str(added_eval) + " evaluates! \n and \n" + str(added_ques) + " Questions!"
+	message.text = "\nCONGRATS\n You get a \n" + str(added_eval) + " evaluates! \n and \n" + str(added_ques) + " Questions!"
 	accept_btn.show()
 	ach_continue_btn.hide()
 
 func _on_accept_pressed() -> void:
-	message.text =  "Congrats  For Completing\n Your First Day\n You're now ready for more \n harder days \n to come.\n"
+	message.text =  "\nCongrats  For Completing\n Your First Day\n You're now ready for more \n harder days \n to come.\n"
 	continue_btn.show()
 	accept_btn.hide()
 
